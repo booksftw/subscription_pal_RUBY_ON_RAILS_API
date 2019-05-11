bbb9ffdd20ae4ad82cb3b312cacc2520bead995c
Nicholas Zachary <nicholas.zachary@outlook.com>
2019-05-10 21:12:14 -0700

    base api set up

---
+++ db/migrate/20190511032641_create_subscriptions.rb
@@ -0,0 +1,10 @@
+class CreateSubscriptions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :subscriptions do |t|
+
+      t.timestamps
+    t.string :name
+    t.integer :amount
+    end
+  end
+end
